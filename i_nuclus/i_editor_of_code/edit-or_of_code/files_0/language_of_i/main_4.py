d16eb30b0add74b2e5e6393a23cefa012172aed0
IPrincipalCentral1 <deboubil22@gmail.com>
2025-06-13 16:03:44 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/language_of_i/main_4.py
@@ -0,0 +1,224 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+os.system("pip install requests")
+
+
+
+
+
+
+
+list_of_link = [
+
+
+            r"https://github.com/UniversalDependencies/UD_English-GUM/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-EWT/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-ParTUT/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-GENTLE/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-LinES/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-PUD/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-Pronouns/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-CHILDES/tree/dev",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-GUMReddit/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-Atis/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-ESLSpok/tree/master",
+            
+            
+            r"https://github.com/UniversalDependencies/UD_English-CTeTex/tree/master",
+            
+            
+            
+            
+            ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_link_of_repozitory = []
+
+
+counter_0 = 0
+
+
+while (counter_0 < len(list_of_link)):
+
+
+
+
+
+    if (list_of_link[counter_0].startswith("https://github.com/")):
+    
+        
+        v_0 = list_of_link[counter_0].split("https://github.com/")
+    
+        v_1 = v_0[-1].split("/")
+        
+        link = "https://github.com/" + v_1[0] + "/" + v_1[1] + ".git"
+    
+        if (not link in list_of_link_of_repozitory):
+    
+        
+            list_of_link_of_repozitory.append(link)
+        
+        
+            print(f"list_of_link_of_repozitory[{counter_0}] = {list_of_link_of_repozitory[counter_0]} .")
+    
+    
+    
+    
+    elif (list_of_link[counter_0].startswith("http://github.com/")):
+    
+        v_0 = list_of_link[counter_0].split("http://github.com/")
+    
+        v_1 = v_0[-1].split("/")
+        
+        link = "http://github.com/" + v_1[0] + "/" + v_1[1] + ".git"
+    
+        if (not link in list_of_link_of_repozitory):
+    
+            list_of_link_of_repozitory.append(link)
+
+    
+            print(f"list_of_link_of_repozitory[{counter_0}] = {list_of_link_of_repozitory[counter_0]} .")
+
+
+    counter_0 += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import requests
+
+import zipfile
+
+import io
+
+
+# https://github.com/IPrincipalCentral/i_principal_central   for money 0
+
+
+
+
+
+url = list_of_link_of_repozitory[0]
+
+
+folder_0 = os.path.join(os.getcwd(), "space_of_language_0", "new_folder")
+
+
+
+
+try:
+    
+    
+    
+    
+    
+
+    response = requests.get(url)
+    
+    
+    if response.status_code == 200:
+    
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+    
+            zip_ref.extractall(folder_0)
+    
+
+
+    
+
+except:    
+
+    semaphore = True
+
+    print("finish : ", f"Download Fail-ed: {e} ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
