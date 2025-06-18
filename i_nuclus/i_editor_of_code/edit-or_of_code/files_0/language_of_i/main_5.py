49f191e05c385dbcb41f0c9b8a7fccddfc5e9d13
IPrincipalCentral1 <deboubil22@gmail.com>
2025-06-18 17:54:01 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/language_of_i/main_5.py
@@ -0,0 +1,166 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+from pathlib import Path
+
+
+
+
+
+
+
+list_of_word_with_definition = []
+
+
+content = ""
+
+
+
+folder_0 = os.path.join(os.getcwd(), "space_0", "space_of_language_2")
+
+
+
+file_0 = os.path.join(folder_0, "definition_of_word_0.txt")
+
+
+
+
+
+d_0 = Path(file_0)
+
+
+content = d_0.read_text()
+
+
+
+
+
+v_0 = content.split("\n")
+
+
+
+counter_0 = 0
+
+while (counter_0 < len(v_0)):
+
+    if ((v_0[counter_0] != "")):
+    
+    
+        if (v_0[counter_0][0] != " "):
+        
+            counter_1 = 0
+            
+            while ((counter_1 < len(v_0[counter_0])) and (v_0[counter_0][counter_1] != " ")):
+            
+                counter_1 += 1
+            
+            
+            content = ""
+            
+            
+            counter_1 += 1
+            
+            
+            while ((counter_1 < len(v_0[counter_0])) and (v_0[counter_0][counter_1] != " ")):
+            
+                content += v_0[counter_0][counter_1]
+                
+                counter_1 += 1
+                
+            
+            list_of_word_with_definition.append([content, []])
+            
+
+
+        else:
+        
+                        
+            counter_1 = 0
+            
+            while ((counter_1 < len(v_0[counter_0])) and (v_0[counter_0][counter_1] == " ")):
+            
+                counter_1 += 1
+            
+            
+            content = ""
+            
+            
+
+            
+            while ((counter_1 < len(v_0[counter_0])) and (v_0[counter_0][counter_1] != "\n")):
+            
+                content += v_0[counter_0][counter_1]
+                
+                counter_1 += 1
+                
+            
+            list_of_word_with_definition[-1][1].append(content)
+            
+            
+
+    counter_0 += 1
+
+
+
+
+
+
+
+
+counter_0 = 0
+
+
+number_0 = len(list_of_word_with_definition)
+
+number_0 = 20
+
+
+while (counter_0 < number_0):
+
+
+    print(f"{counter_0}. {list_of_word_with_definition[counter_0][0]} :")
+    
+    
+    counter_1 = 0
+    
+    
+    while (counter_1 < len(list_of_word_with_definition[counter_0][1])):
+    
+        
+        print(f"    {list_of_word_with_definition[counter_0][1][counter_1]}")
+    
+        counter_1 += 1
+
+
+
+    counter_0 += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
