45e4e85a4a38fd091af965eb62610d6efe47b6c2
IPrincipalCentral1 <deboubil22@gmail.com>
2025-06-24 21:27:07 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_0/i_tester_0.py
@@ -0,0 +1,606 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+
+                            ["requests"] ,
+                            
+                            
+
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+
+import sys
+
+import subprocess
+
+
+
+
+counter_0 = 0
+
+
+while (counter_0 < len(list_of_liberary_to_install)):
+
+        
+    try:
+    
+    
+        print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+    
+        #os.system(f"pip3 install {list_of_liberary_to_install[counter_0][0]}")
+    
+        
+        
+        subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+    
+    
+            
+    except:
+    
+            
+                    
+        traceback.print_exc()
+        
+        error = traceback.format_exc()
+        
+        semaphore = True
+        
+        print(f"Erreur : {str(error)}")
+        
+    
+    
+    counter_0 += 1
+    
+
+
+
+import requests
+
+
+
+import time
+
+
+
+
+
+
+
+
+def get_paypal_like_rate(from_currency, to_currency, paypal_margin=0.05):
+
+
+
+    url = f"https://open.er-api.com/v6/latest/{from_currency}"
+
+
+    response = requests.get(url)
+
+    print("Status:", response.status_code)
+
+    data = response.json()
+
+
+    if "rates" not in data or to_currency not in data["rates"]:
+        raise Exception("فشل في الحصول على سعر الصرف.")
+
+
+    real_rate = data["rates"][to_currency]
+
+    paypal_rate = real_rate * (1 - paypal_margin)
+
+    extract_ed = real_rate * paypal_margin
+
+    return real_rate, extract_ed, paypal_rate
+
+# تجربة
+
+
+
+
+
+def mixer_0(list_0, file):
+
+    list_1 = [0, 0]
+
+
+    #file_0 = os.path.join(os.getcwd(), "currency_0.csv")
+
+    with open(file, "w") as f_:
+    
+        
+    
+        counter_0 = 0
+        
+        while (counter_0 < len(list_0)):
+        
+            
+            counter_1 = 0
+            
+            while (counter_1 < len(list_0)):
+                
+                
+                            
+                try:
+                
+                
+                    real, extract_ed, paypal_like = get_paypal_like_rate(list_0[counter_0], list_0[counter_1])
+                
+                    print(f" 1 {list_0[counter_0]} = {real:.2f} {list_0[counter_1]} (PayPal ≈ {paypal_like:.2f} {list_0[counter_1]})")
+                
+                    f_.write(f"1;{list_0[counter_0]};{real:.2f};{list_0[counter_1]};bank;{paypal_like:.2f};{list_0[counter_1]};extract-ed;{extract_ed:.2f}\n")
+                
+                
+                except Exception as e:
+                
+                
+                    print(f"❌ فشل التحويل إلى : {e}")
+                
+                
+                
+                counter_1 += 1
+            
+            
+            counter_0 += 1
+        
+    
+    
+def get_from_file(file):
+
+
+    list_of_result = []
+
+    with open(file, "r") as f_:
+    
+        content = f_.read(os.path.getsize(file))
+    
+    
+    v_0 = content.split("\n")
+    
+    counter_0 = 0
+    
+    while (counter_0 < len(v_0)):
+    
+    
+        if (v_0[counter_0] != ""):
+        
+            v_2 = v_0[counter_0].split(";")
+        
+            s_0 = v_2[5]
+            
+            v_1 = s_0.split(".")
+            
+            n_0 = int(v_1[0]) + (int(v_1[1]) / (10 ** 2))
+            
+            list_of_result.append([v_2[1], n_0, v_2[3]])
+            
+        counter_0 += 1
+
+    return list_of_result
+
+
+def finder_0(list_0, element):
+
+    counter_0 = 0
+    
+    while ((counter_0 < len(list_0)) and (list_0[counter_0][0] != element)):
+    
+        counter_0 += 1
+
+    
+    return counter_0
+
+
+def finder_1(list_0, element_0, element_1):
+
+    counter_0 = 0
+    
+    while ((counter_0 < len(list_0)) and (list_0[counter_0][0] != element_0)):
+    
+        counter_0 += 1
+
+    if (counter_0 < len(list_0)):
+    
+                
+        while ((counter_0 < len(list_0)) and (list_0[counter_0][2] != element_1)):
+        
+            counter_0 += 1
+            
+    else:
+    
+        counter_0 = len(list_0)
+        
+        
+    
+    return counter_0
+
+
+
+
+def transformer_0(list_0, unity_0, unity_1, amount):
+
+    counter_0 = finder_1(list_0=list_0, element_0=unity_0, element_1=unity_1)
+    
+    semaphore_of_error = False
+
+    amount_of_result = 0.0
+
+    if ((counter_0 < len(list_0))):
+    
+        amount_of_result = amount * list_0[counter_0][1]
+        
+    else:
+    
+        semaphore_of_error = True
+        
+    
+    return  [semaphore_of_error, amount_of_result]
+
+
+
+
+def transform_and_calculate_0(list_0, amount):
+    
+    
+    
+        
+    
+    list_of_unity = list_0
+    
+    
+    
+    result_1 = [False, amount]
+    
+    
+    print(f"\n\n list_of_unity = {list_of_unity} \n\n result_1 = {result_1} \n\n")
+    
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 + 1 < len(list_of_unity)):
+    
+        
+        result_1 = transformer_0(list_0=list_of_result, unity_0=list_of_unity[counter_0], unity_1=list_of_unity[counter_0 + 1], amount=result_1[1])
+        
+        counter_0 += 1
+    
+    
+    
+    print(f"\n\n\n new : result_1 = {result_1} \n\n\n")
+    
+    
+    return result_1
+    
+
+
+
+
+
+
+
+def int_to_str_0(number_0):
+
+    str_0 = str(number_0)
+    
+    counter_0 = len(str_0)
+    
+
+
+    str_result = str_0
+    
+
+    if (18 > len(str_0)):
+    
+    
+        counter_1 = 0
+    
+        while (counter_1 < 18 - counter_0):
+            
+            str_result = "0" + str_result
+            
+            counter_1 += 1
+        
+    
+    
+    return str_result
+
+
+
+
+
+wise_supported_currencies = [
+    
+    "EUR",  # Euro        
+    "AED",  # UAE Dirham
+    "AUD",  # Australian Dollar
+    "BDT",  # Bangladeshi Taka
+    "BGN",  # Bulgarian Lev
+    "BRL",  # Brazilian Real
+    "CAD",  # Canadian Dollar
+    "CHF",  # Swiss Franc
+    "CLP",  # Chilean Peso
+    "CNY",  # Chinese Yuan
+    "CRC",  # Costa Rican Colón
+    "CZK",  # Czech Koruna
+    "DKK",  # Danish Krone
+    "EGP",  # Egyptian Pound
+    "GBP",  # British Pound
+    "GEL",  # Georgian Lari
+    "HKD",  # Hong Kong Dollar
+    "HRK",  # Croatian Kuna
+    "HUF",  # Hungarian Forint
+    "IDR",  # Indonesian Rupiah
+    "ILS",  # Israeli New 
+    "INR",  # Indian Rupee
+    "JPY",  # Japanese Yen
+    "KES",  # Kenyan Shilling
+    "KRW",  # South Korean Won
+    "LKR",  # Sri Lankan Rupee
+    "MAD",  # Moroccan Dirham
+    "MXN",  # Mexican Peso
+    "MYR",  # Malaysian Ringgit
+    "NGN",  # Nigerian Naira
+    "NOK",  # Norwegian Krone
+    "NZD",  # New Zealand Dollar
+    "PEN",  # Peruvian Sol
+    "PHP",  # Philippine Peso
+    "PKR",  # Pakistani Rupee
+    "PLN",  # Polish Zloty
+    "RON",  # Romanian Leu
+    "RUB",  # Russian Ruble
+    "SAR",  # Saudi Riyal
+    "SEK",  # Swedish Krona
+    "SGD",  # Singapore Dollar
+    "THB",  # Thai Baht
+    "TRY",  # Turkish Lira
+    "TZS",  # Tanzanian Shilling
+    "UAH",  # Ukrainian Hryvnia
+    "UGX",  # Ugandan Shilling
+    "USD",  # US Dollar
+    "VND",  # Vietnamese Dong
+    "ZAR"   # South African Rand
+
+]
+
+
+
+target_currencies = ["EUR", "GBP", "JPY", "CAD", "AUD", "CHF", "USD"]
+
+
+paypal_supported_currencies = [
+
+                            "EUR", "AUD", "BRL", "CAD", "CNY", "CZK", "DKK", "HKD",
+
+                            "HUF", "INR", "ILS", "JPY", "MYR", "MXN", "TWD", "NZD",
+
+                            "NOK", "PHP", "PLN", "GBP", "RUB", "SGD", "SEK", "CHF",
+
+                            "THB", "TRY", "USD"
+
+                            ]
+
+
+
+
+t1 = time.time()
+
+
+file_0 = os.path.join(os.getcwd(), "currency_0.csv")
+
+#mixer_0(list_0=wise_supported_currencies, file=file_0)
+
+list_of_result = get_from_file(file=file_0)
+
+
+
+t2 = time.time()
+
+
+
+print(f"\n\n\ntime = {t2 - t1} second .\n\n\n")
+
+n_0 = 1
+
+print(f"list_of_result[{n_0}] = {list_of_result[n_0]}")
+
+
+
+
+'''
+
+1;USD;0.87;EUR;PayPal;0.82;EUR;extract-ed;0.04
+
+1;EUR;1.15;USD;PayPal;1.10;USD;extract-ed;0.06
+
+
+1 USD = 0.82 EUR
+
+1 EUR = 1.15 USD
+
+
+
+
+
+'''
+
+
+
+
+list_of_unity = ["EUR", "USD", "AUD", "EUR"]
+
+amount = 5.0
+
+
+result_1 = transform_and_calculate_0(list_0=list_of_unity, amount=amount)
+
+
+
+
+list_of_unity = ["EUR", "AUD", "USD", "EUR"]
+
+
+
+
+result_1 = transform_and_calculate_0(list_0=list_of_unity, amount=amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+-----------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------
+
+
+
+start
+
+
+
+
+'''
+
+print("\n\nstart :")
+
+
+
+os.system("python3 refresher_0.py")
+
+
+
+
+list_of_file = []
+
+
+counter_0 = 0
+
+
+while (counter_0 < 3):
+
+    
+    
+    str_number_0 = int_to_str_0(number_0=counter_0)
+    
+    file_0 = os.path.join(os.getcwd(), "space_for_mix", f"file_part_{str_number_0}.mixer")
+
+    print(f"\n file_0 = {file_0} .\n")
+
+    with open(file_0, "w") as f_:
+    
+        pass
+
+    counter_0 += 1
+
+
+
+
+file_1 = os.path.join(os.getcwd(), "Economic_Partner_official_produced_mixer_8.c")
+
+
+with open(file_1, "r") as f_:
+
+    content = f_.read(os.path.getsize(file_1))
+
+
+
+
+content = content.replace("___number_of_chunk___", "10")
+
+
+
+content = content.replace("___postion_of_max_range___", str(len(list_of_unity) - 1))
+
+
+
+file_2 = os.path.join(os.getcwd(), "Economic_Partner_official_produced_mixer_8_0.c")
+
+
+with open(file_2, "w") as f_:
+
+    f_.write(content)
+
+
+
+
+
+
+
+os.system("gcc Economic_Partner_official_produced_mixer_8_0.c -o E_P_o_p_mixer_8_0")
+
+
+
+
+
+
+
+
+
+print(f"finish .\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
