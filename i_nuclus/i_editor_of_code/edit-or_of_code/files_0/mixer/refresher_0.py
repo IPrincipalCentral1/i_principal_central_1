28c2718cbd6549ace43cb4e3e370023c35006718
IPrincipalCentral1 <deboubil22@gmail.com>
2025-07-08 08:08:22 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/mixer/refresher_0.py
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+this file should be executed before . if you want to launch a new mix from the beginning
+
+
+
+'''
+
+
+
+import os
+
+folder_0 = os.path.join(os.getcwd(), "space_for_mix")
+
+
+if (os.path.exists(folder_0)):
+    
+    
+    
+    
+    
+    
+    
+    
+    files = []
+    
+    for root, dirs, files in os.walk(folder_0):
+    
+        break
+    
+    
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(files)):
+    
+        
+        os.remove(os.path.join(folder_0, files[counter_0]))
+    
+    
+        counter_0 += 1
+    
+    
+        
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(dirs)):
+    
+        
+        os.rmdir(os.path.join(folder_0, dirs[counter_0]))
+    
+    
+        counter_0 += 1
+    
+    
+    
+    
+    
+else:
+
+
+    os.makedirs(dist_, exist_ok=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
