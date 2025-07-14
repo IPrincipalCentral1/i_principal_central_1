6fbfdaf8951b4e5c3f810ee8773342172b8d53ec
IPrincipalCentral1 <deboubil22@gmail.com>
2025-07-14 15:39:05 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/all_my_i_0/space_0/all_what_i_have/generator_of_what_i_have_0.py
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+
+
+
+
+
+
+
+list_of_what_i_have = [
+                    
+                    
+                    [["name_of_thing", "computer"], ["amount", "1.0"]],
+                    
+                    
+                    [["name_of_thing", "phone"], ["amount", "1.0"], ["detail", "Huawei"], ["number_of_phone", "+213 561577437"]],
+                    
+                    
+                    [["name_of_thing", "water to drink"], ["amount", ""]],
+                    
+                    
+                    [["name_of_thing", "water to clean"], ["amount", ""]],
+                    
+                    
+                    [["name_of_thing", "electricity"], ["amount", ""]],
+                    
+                    
+                    
+                    ]
+
+
+
+
+
+
+
+
+
+
+file_0 = os.path.join(os.getcwd(), "all_what_i_have_0.txt")
+
+
+
+
+
+with open(file_0, "w") as f_:
+    
+    
+    f_.write("\n" * 10)
+    
+    
+    f_.write("i have : \n")
+    
+    counter_3 = 1
+    
+    
+    f_.write("\n" * counter_3)
+
+    counter_0 = 0
+    
+    while (counter_0 < len(list_of_what_i_have)):
+        
+        
+        counter_1 = 0
+        
+        
+        
+        f_.write(f"    {list_of_what_i_have[counter_0][counter_1][0]} : \"{list_of_what_i_have[counter_0][counter_1][1]}\" \n")
+        
+        f_.write("\n" * counter_3)
+        
+        
+        counter_1 += 1
+        
+        
+        while (counter_1 < len(list_of_what_i_have[counter_0])):
+        
+                        
+            f_.write(f"        {list_of_what_i_have[counter_0][counter_1][0]} : \"{list_of_what_i_have[counter_0][counter_1][1]}\" \n")
+            
+            f_.write("\n" * counter_3)
+            
+            
+            counter_1 += 1
+        
+        
+        counter_0 += 1
+    
+
+
+
+
+
+
+
+
+
+
+
