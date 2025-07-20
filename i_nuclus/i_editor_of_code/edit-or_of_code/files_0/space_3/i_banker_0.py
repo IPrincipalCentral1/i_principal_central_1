d85b71b32d28e01ef763cb7651de2f9fbb43c277
IPrincipalCentral1 <deboubil22@gmail.com>
2025-07-20 16:59:24 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_3/i_banker_0.py
@@ -0,0 +1,224 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+import traceback
+
+import sys
+
+
+
+
+type_of_encoding_0 = "utf-8"
+
+
+
+
+
+
+def create_personal_account(name_of_file_of_the_bank, identificater, name, pre_name, number_of_phone, e_mail, pass_word):
+    
+    
+    if (os.path.exists(name_of_file_of_the_bank) == False):
+        
+        with open(name_of_file_of_the_bank, "w", encoding=type_of_encoding_0) as f_:
+            
+            
+            f_.write(f"i_bank : \n\n")
+            
+            number_of_tab = 1
+            
+            f_.write(" " * (4 * number_of_tab) + f"identificater : \"{identificater}\" \n\n")
+            
+                        
+            number_of_tab += 1
+            
+            f_.write(" " * (4 * number_of_tab) + f"name : \"{name}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"pre_name : \"{pre_name}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"number_of_phone : \"{number_of_phone}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"e_mail : \"{e_mail}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"pass_word : \"{pass_word}\" \n\n")
+            
+            
+            f_.write(" " * (4 * number_of_tab) + f"pocket : \n\n")
+            
+            
+            
+    else:
+        
+        
+        content_0 = ""
+        
+        with open(name_of_file_of_the_bank, "r", encoding=type_of_encoding_0) as f_:
+            
+            content_0 = f_.read(os.path.getsize(name_of_file_of_the_bank))
+            
+                
+        with open(name_of_file_of_the_bank, "w", encoding=type_of_encoding_0) as f_:
+            
+            
+            f_.write(content_0 + "\n")
+            
+            number_of_tab = 1
+            
+            f_.write(" " * (4 * number_of_tab) + f"identificater : \"{identificater}\" \n\n")
+            
+                        
+            number_of_tab += 1
+            
+            f_.write(" " * (4 * number_of_tab) + f"name : \"{name}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"pre_name : \"{pre_name}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"number_of_phone : \"{number_of_phone}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"e_mail : \"{e_mail}\" \n\n")
+            
+            f_.write(" " * (4 * number_of_tab) + f"pass_word : \"{pass_word}\" \n\n")
+            
+            
+            f_.write(" " * (4 * number_of_tab) + f"pocket : \n\n")
+            
+            
+            
+            
+
+
+def main():
+
+    
+    
+    
+    try:
+        
+        
+        
+        
+        
+        
+        '''
+        
+        
+        comment :
+            
+            
+            function_0 :
+                
+                create_personal_account
+            
+            function_1 : 
+                
+                create_new_account_with_new_unity
+                
+            
+            function_2 : 
+                
+                affect_amount_to_the_account_with_a_specific_unity
+                
+            function_3 :
+                
+                add_amount_to_the_account_with_a_specific_unity
+            
+                        
+            function_4 :
+                
+                substrut_amount_to_the_account_with_a_specific_unity
+                
+            
+            function_5 :
+                
+                transfer_amount_from_an_account_to_another_with_gain
+                
+            function_6 :
+                            
+                making_the_mesurment_between_unitys
+            
+                        
+            function_7 :
+            
+                
+                transforming_from_a_unity_to_another
+                
+            
+        '''
+        
+        
+        
+        
+        
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        
+        
+        
+        file_0 = os.path.join(cwd, "bank_0.the_bank")
+        
+        create_personal_account(name_of_file_of_the_bank=file_0, identificater="0", name="billal", pre_name="debouci", number_of_phone="+213 123456789", e_mail="e_mil_0@e_mail.com", pass_word="i_male_principal_central_pass_word_0")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+    except:
+    
+            
+                    
+        traceback.print_exc()
+        
+        error = traceback.format_exc()
+        
+        semaphore = True
+        
+        print(f"Erreur : {str(error)}")
+        
+    
+        
+        
+        
+    
+    
+    
+    
+    
+
+
+if __name__ == "__main__":
+
+
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
