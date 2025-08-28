21f15596454e107e5d05d3cfed61c13e67f562da
IPrincipalCentral1 <deboubil22@gmail.com>
2025-08-28 16:55:37 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_1/i_reformater_0.py
@@ -0,0 +1,193 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+encoding_0 = "utf-8"
+
+
+
+
+def i_function_reformater_0(name_of_file):
+    
+    
+    
+    result_0 = "\""
+    
+    content_0 = ""
+    
+    with open(name_of_file, "r", encoding=encoding_0) as f_:
+        
+        content_0 = f_.read(os.path.getsize(name_of_file))
+        
+        
+    
+    content_0 = content_0.replace("\n", "\\n")
+    
+    content_0 = content_0.replace("\"", "\\\"")
+    
+    
+    result_0 += content_0 + "\""
+    
+    
+    
+    return result_0
+    
+ 
+
+
+
+def i_function_reformater_1(the_content_to_make_on_it):
+    
+    
+    
+    result_0 = "\""
+    
+    content_0 = the_content_to_make_on_it
+    
+    
+        
+    
+    content_0 = content_0.replace("\n", "\\n")
+    
+    content_0 = content_0.replace("\"", "\\\"")
+    
+    
+    result_0 += content_0 + "\""
+    
+    
+    
+    return result_0
+    
+    
+
+
+
+
+def i_inverse_reformater_0(the_content_to_make_the_reverse_on_it):
+    
+    
+    
+    
+    
+    content_0 = the_content_to_make_the_reverse_on_it[1:-1]
+        
+    content_0 = content_0.replace("\\n", "\n")
+    
+    content_0 = content_0.replace("\\\"", "\"")
+    
+    
+    
+    
+    result_0 = content_0
+    
+    
+    return result_0
+    
+
+
+
+def i_inverse_reformater_1(name_of_file):
+    
+    
+        
+    content_0 = ""
+    
+    with open(name_of_file, "r", encoding=encoding_0) as f_:
+        
+        content_0 = f_.read(os.path.getsize(name_of_file))
+        
+        
+    
+    
+    content_0 = content_0[1:-1]
+        
+    content_0 = content_0.replace("\\n", "\n")
+    
+    content_0 = content_0.replace("\\\"", "\"")
+    
+    
+    
+    
+    result_0 = content_0
+    
+    
+    return result_0
+    
+    
+    
+
+def main():
+    
+    
+    
+    
+    file_0 = "/mnt/mydisk/shared_directory_on_PC/my_link_updated/i_directory/main_editor_of_code/edit-or_of_code_for_program/edit-or_of_code/files_0/space_1/file_0.txt"
+    
+    
+    
+    
+    
+    
+    result_0 = i_function_reformater_0(name_of_file=file_0)
+    
+    
+    print(f"result_0 = {result_0} .")
+    
+    
+    result_1 = i_inverse_reformater(the_content_to_make_the_reverse_on_it=result_0)
+    
+    
+    print(f"result_1 = {result_1} .")
+    
+    
+    
+    with open(file_0, "r", encoding=encoding_0) as f_:
+        
+        content_0 = f_.read(os.path.getsize(file_0))
+        
+    
+    print(f"(content_0 == result_1) = {content_0 == result_1}")
+    
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    main()
+    
+    
+    
+
+
+
+
+
+
+
+
