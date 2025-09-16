f3689f2a4097548648b74dececa49ee9bc31b950
IPrincipalCentral1 <deboubil22@gmail.com>
2025-09-16 17:15:28 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_1/i_reformater_2.py
@@ -0,0 +1,149 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+                            
+                            ["psutil"] ,
+                            
+                            
+
+
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+import traceback
+
+import sys
+
+
+import subprocess
+
+
+
+
+
+try:
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(list_of_liberary_to_install)):
+    
+    
+        try:
+    
+        
+            print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+            
+            
+            
+            
+            subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+        
+    
+                
+        except:
+        
+                
+                        
+            traceback.print_exc()
+            
+            error = traceback.format_exc()
+            
+            semaphore = True
+            
+            print(f"Erreur : {str(error)}")
+            
+    
+    
+        counter_0 += 1
+    
+    
+except:
+
+        
+                
+    traceback.print_exc()
+    
+    error = traceback.format_exc()
+    
+    semaphore = True
+    
+    print(f"Erreur : {str(error)}")
+    
+    
+
+
+
+print("\n" * 10)
+
+
+
+
+
+
+
+import psutil
+
+
+
+def bytes_to_gb(b):
+
+    return b / (1024**3)
+
+
+vm = psutil.virtual_memory()
+
+
+print(f"Total RAM:     {bytes_to_gb(vm.total):.2f} GB")
+
+print(f"Available RAM: {bytes_to_gb(vm.available):.2f} GB")
+
+print(f"Used RAM:      {bytes_to_gb(vm.used):.2f} GB")
+
+print(f"Free RAM:      {bytes_to_gb(vm.free):.2f} GB")
+
+print(f"Usage percent: {vm.percent}%")
+
+
+print("\n" * 10)
+
+os.system(f"free -h")
+
+
+
+
+
+
+
+
+
+
+
+
+
