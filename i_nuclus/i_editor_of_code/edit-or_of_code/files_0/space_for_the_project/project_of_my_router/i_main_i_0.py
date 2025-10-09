0337ee5cb8769fe48ccede448de976ac07fe5632
IPrincipalCentral1 <deboubil22@gmail.com>
2025-10-09 21:24:14 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_router/i_main_i_0.py
@@ -0,0 +1,348 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+
+                            ["PyQt5"] ,
+                            
+                            
+                            ["psutil"] ,
+                            
+                            
+                            ["requests"] ,
+                            
+                            
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+import traceback
+
+import sys
+
+
+import subprocess
+
+
+
+try:
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(list_of_liberary_to_install)):
+    
+
+                
+        try:
+        
+        
+            print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+            
+            subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+        
+        
+                
+        except:
+        
+                
+                        
+            traceback.print_exc()
+            
+            error = traceback.format_exc()
+            
+            semaphore = True
+            
+            print(f"Erreur : {str(error)}")
+            
+        
+        
+        counter_0 += 1
+        
+        
+    
+except:
+
+        
+                
+    traceback.print_exc()
+    
+    error = traceback.format_exc()
+    
+    semaphore = True
+    
+    print(f"Erreur : {str(error)}")
+    
+    
+    
+
+
+print("\n" * 10)
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+class i_simulation_of_router_0_i():
+    
+    
+    def __init__(self):
+        
+        
+        self.i_content_of_json_0_i = "\n\n[\n\n]"
+        
+        
+        
+        
+        
+    
+    
+    def i_create_router_0_i(self, i_identificator_0_i, i_name_0_i, i_detail_0_i):
+        
+        
+        
+        i_counter_0_i = len(self.i_content_of_json_0_i) - 1
+        
+        while ((i_counter_0_i >= 0) and (self.i_content_of_json_0_i[i_counter_0_i] != "]")):
+            
+            i_counter_0_i -= 1
+            
+            
+            
+        
+        self.i_content_of_json_0_i = self.i_content_of_json_0_i[:i_counter_0_i]
+        
+        
+        i_v_0_i = (self.i_content_of_json_0_i).split("}")
+        
+        if (len(i_v_0_i) == 1):
+            
+            
+            self.i_content_of_json_0_i += "\n    { \n"
+            
+        else:
+            
+            
+            self.i_content_of_json_0_i += "\n     , \n"
+            
+            self.i_content_of_json_0_i += "\n    { \n"
+            
+            
+            
+        
+        
+        
+        self.i_content_of_json_0_i += "\n" + f"        \"i_identificator_0_i\" : \"{i_identificator_0_i}\" , " + "\n"
+        
+        
+        self.i_content_of_json_0_i += "\n" + f"        \"i_name_0_i\" : \"{i_name_0_i}\" , " + "\n"
+        
+        
+        self.i_content_of_json_0_i += "\n" + f"        \"i_detail_0_i\" : \"{i_detail_0_i}\"  " + "\n"
+        
+        
+        self.i_content_of_json_0_i += "\n    } \n"
+        
+            
+        
+        self.i_content_of_json_0_i = "\n\n]"
+        
+    
+    
+    
+
+
+
+
+
+
+def i_main_0_i():
+    
+    
+    
+    
+    i_class_0_i = i_simulation_of_router_0_i()
+    
+    
+    i_class_0_i.i_create_router_0_i(i_identificator_0_i="0", i_name_0_i="i_router_0_i", i_detail_0_i="{\\\"ip\\\" : \\\"127.0.0.1\\\" , \\\"port\\\" : \\\"5000\\\"}")
+    
+    
+    i_content_0_i = i_class_0_i.i_content_of_json_0_i
+    
+    
+    print(f"i_content_0_i = {i_content_0_i} .")
+    
+    
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    
+    i_main_0_i()
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
