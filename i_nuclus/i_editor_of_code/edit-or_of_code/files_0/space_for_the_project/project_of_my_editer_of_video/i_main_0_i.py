47496493c7e476e5adf66eb7bafc53bde08b4900
IPrincipalCentral1 <deboubil22@gmail.com>
2025-10-25 18:29:26 +0100

    i_hello

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_editer_of_video/i_main_0_i.py
@@ -0,0 +1,492 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+                            
+                            
+                            ["pillow"] ,
+                            
+                            
+                            ["manim"] ,
+                            
+                            
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+import traceback
+
+import sys
+
+
+import subprocess
+
+
+
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+print(f"\n\n    pip install --upgrade pip setuptools wheel \n\n\n")
+
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"])
+
+
+
+try:
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(list_of_liberary_to_install)):
+    
+    
+        try:
+            
+            
+            
+            
+            print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+            
+            #os.system(f"pip3 install {list_of_liberary_to_install[counter_0][0]}")
+            
+            
+            
+            subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+        
+        
+                
+        except:
+        
+                
+                        
+            traceback.print_exc()
+            
+            error = traceback.format_exc()
+            
+            semaphore = True
+            
+            print(f"Erreur : {str(error)}")
+            
+    
+    
+        counter_0 += 1
+    
+    
+except:
+
+        
+                
+    traceback.print_exc()
+    
+    error = traceback.format_exc()
+    
+    semaphore = True
+    
+    print(f"Erreur : {str(error)}")
+    
+    
+
+
+
+print("\n" * 10)
+
+
+
+
+
+
+global i_content_0_i
+
+
+
+i_content_0_i = r"""
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+                            
+                            
+                            ["pillow"] ,
+                            
+                            
+                            ["manim"] ,
+                            
+                            
+                            ["manimgl"] ,
+                            
+                            
+                            ["PyOpenGL_accelerate"] ,
+                            
+                            
+                            ["PyOpenGL"] ,
+                            
+                            
+                            ["pygame"] ,
+                            
+                            
+                            
+                            
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+import traceback
+
+import sys
+
+
+import subprocess
+
+
+
+
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+print(f"\n\n    pip install --upgrade pip setuptools wheel \n\n\n")
+
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"])
+
+
+
+try:
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(list_of_liberary_to_install)):
+    
+    
+        try:
+            
+            
+            
+            print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+            
+            #os.system(f"pip3 install {list_of_liberary_to_install[counter_0][0]}")
+            
+            
+            
+            subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+        
+    
+                
+        except:
+        
+                
+                        
+            traceback.print_exc()
+            
+            error = traceback.format_exc()
+            
+            semaphore = True
+            
+            print(f"Erreur : {str(error)}")
+            
+    
+    
+        counter_0 += 1
+    
+    
+except:
+
+        
+                
+    traceback.print_exc()
+    
+    error = traceback.format_exc()
+    
+    semaphore = True
+    
+    print(f"Erreur : {str(error)}")
+    
+    
+
+
+
+print("\n" * 10)
+
+
+
+
+
+
+i_folder_0_i = os.path.join(cwd, "i_space_for_library_0_i")
+
+
+sys.path.append(i_folder_0_i)
+
+
+
+import i_main_of_library_0_i
+
+
+
+
+
+'''
+
+the part of the code .
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+from manimlib import *
+
+class _____name_of_class_0_____(ThreeDScene):
+    def construct(self):
+        circle = Circle(radius=2, color=BLUE)
+        circle.move_to(OUT * 1)
+        self.add(circle)
+
+# لتشغيلها تفاعليًا:
+# اكتب في الطرفية:
+# manimgl file_name.py InteractiveScene
+    
+    
+
+
+
+"""
+
+
+
+
+
+def i_main_0_i():
+    
+    
+    
+    global i_content_0_i
+    
+    
+    
+    i_name_of_project_0_i = "i_program_0_i"
+    
+    
+    i_content_0_i = i_content_0_i.replace("_____name_of_class_0_____", i_name_of_project_0_i)
+    
+    
+    
+    
+    i_name_of_file_0_i = "i_main_0_0_i.py"
+    
+    i_file_0_i = os.path.join(cwd, i_name_of_file_0_i)
+    
+    with open(i_file_0_i, "w") as f_:
+        
+        f_.write(i_content_0_i)
+        
+    
+    
+    #os.system(f"{sys.executable} -m manim -pql {i_name_of_file_0_i} {i_name_of_project_0_i}")
+    
+    
+    os.system(f"{sys.executable} -m manimgl {i_name_of_file_0_i} {i_name_of_project_0_i}")
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+if __name__ == "__main__":
+    
+    
+    
+    i_main_0_i()
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
