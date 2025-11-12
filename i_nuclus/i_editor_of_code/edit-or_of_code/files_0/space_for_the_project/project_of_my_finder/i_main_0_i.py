f83afb8855f87e6564cae0a25be98a30d6a9fef5
IPrincipalCentral1 <deboubil22@gmail.com>
2025-11-12 19:05:31 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_finder/i_main_0_i.py
@@ -0,0 +1,203 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env python3
+"""
+
+lucas_lehmer.py
+
+
+
+
+is : ( ( 2 ** (82_589_933) ) - 1 ) a prime number ?
+
+
+
+
+
+
+تحقق من أن M_p = 2^p - 1 عدد أولي باستخدام اختبار Lucas-Lehmer
+(مناسب فقط للأعداد من شكل ميرسين حيث p يجب أن يكون أولياً).
+
+مثال:
+    python3 lucas_lehmer.py 7
+    python3 lucas_lehmer.py 31
+    
+    
+"""
+
+
+def i_get_binary_0_i(i_number_0_i):
+    
+    
+    i_content_result_0_i = ""
+    
+    
+    i_counter_0_i = 0
+    
+    while (i_number_0_i > 0):
+        
+        
+        i_mod_0_i = i_number_0_i % 2
+        
+        i_number_0_i = i_number_0_i // 2
+        
+        
+        i_content_result_0_i += str(i_mod_0_i)
+        
+        
+    
+    
+    
+    return i_content_result_0_i
+    
+
+
+
+# ---------- واجهة بسيطة ----------
+def main():
+    
+    
+    
+    i_number_0_i = 10
+    
+    
+    i_v_0_i = i_get_binary_0_i(i_number_0_i=i_number_0_i)
+    
+    
+    print(f"i_v_0_i = {i_v_0_i} .")
+    
+    
+    
+    
+if __name__ == "__main__":
+    
+    main()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
