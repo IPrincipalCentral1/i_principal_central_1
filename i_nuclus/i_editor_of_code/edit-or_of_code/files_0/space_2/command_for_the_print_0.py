3d755a3c622f32d96d18ccd3a93487384513da44
IPrincipalCentral1 <deboubil22@gmail.com>
2025-11-17 21:26:50 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_2/command_for_the_print_0.py
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+command to save a print :
+    
+    cnsetuputil2
+    
+    
+    
+    
+
+
+
+
+
+command for printing using an "image.png" as input :
+    
+    
+    link of image = "~/Téléchargements/image.png"
+    
+    
+    command = "lp -d Canon_LBP631C_USB -o fit-to-page -o media=A4 ~/Téléchargements/image.png"
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
