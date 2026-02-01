c99595074aa0cad5b056d631cc27022d4f264cc5
IPrincipalCentral1 <deboubil22@gmail.com>
2026-02-01 07:56:12 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_mixer/i_main_1_i.py
@@ -0,0 +1,223 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def i_next_step_0_i(i_list_0_i, i_number_of_element__minus_1__0_i):
+    
+    
+    
+    
+    i_counter_0_i = 0
+    
+    
+    if (i_counter_0_i < len(i_list_0_i)):
+        
+        
+        i_list_0_i[i_counter_0_i] += 1
+        
+        
+        i_semaphore_of_add_0_i = True
+        
+        
+        while (((i_counter_0_i < len(i_list_0_i))) and (i_semaphore_of_add_0_i == True)):
+            
+            
+            if (i_list_0_i[i_counter_0_i] > i_number_of_element__minus_1__0_i):
+                
+                
+                i_list_0_i[i_counter_0_i] = 0
+                
+                
+                i_counter_0_i += 1
+                
+                
+                i_semaphore_of_add_0_i = True
+                
+                
+            elif (i_counter_0_i > 0):
+                
+                
+                i_list_0_i[i_counter_0_i] += 1
+                
+                
+                i_semaphore_of_add_0_i = False
+                
+                
+            else:
+                
+                
+                i_semaphore_of_add_0_i = False
+                
+                
+                
+        
+        if (i_semaphore_of_add_0_i == True):
+            
+            
+            
+            i_list_0_i.append(0)
+            
+            
+        
+            
+        
+    else:
+        
+        
+        
+        i_list_0_i.append(0)
+        
+        
+        
+        
+    
+    return i_list_0_i
+
+
+
+
+i_max_number_0_i = 3
+
+
+i_list_0_i = [3, 3, 3]
+
+
+
+print(f"i_hello_0_i . i_list_0_i = {i_list_0_i} .")
+
+
+i_list_0_i = i_next_step_0_i(i_list_0_i=i_list_0_i, i_number_of_element__minus_1__0_i=i_max_number_0_i)
+
+
+
+
+
+print(f"i_hello_1_i . i_list_0_i = {i_list_0_i} .")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
