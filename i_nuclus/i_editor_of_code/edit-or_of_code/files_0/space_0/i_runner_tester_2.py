9d802e5d351a232ffdda1420053792e2a3a0a1ea
IPrincipalCentral1 <deboubil22@gmail.com>
2026-07-10 11:27:06 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_0/i_runner_tester_2.py
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+this should be the program the recalculate the way of the money from the beginning .
+
+this will work on linux ubuntu 24.04.2 LTS .
+
+
+
+
+'''
+
+
+
+
+
+
+
+import os
+
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+os.system("python3 i_tester_0.py")
+
+
+os.system("python3 i_tester_1.py init")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
