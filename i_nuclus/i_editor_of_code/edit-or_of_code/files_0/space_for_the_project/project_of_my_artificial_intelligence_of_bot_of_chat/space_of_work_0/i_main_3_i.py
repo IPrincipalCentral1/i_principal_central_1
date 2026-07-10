1f9a124f2759b0f51a6507fddb61ce7d5754f0e6
IPrincipalCentral1 <deboubil22@gmail.com>
2026-07-10 12:02:39 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_artificial_intelligence_of_bot_of_chat/space_of_work_0/i_main_3_i.py
@@ -0,0 +1,264 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+import time
+
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+
+def i_transforme_0_i(hex_value):
+    
+    
+    i_char_0_i = chr(int(hex_value, 16))
+    
+    return i_char_0_i
+    
+    
+
+
+
+
+
+i_counter_1_i = 0
+
+
+i_counter_2_i = 0
+
+
+
+
+
+
+i_t_1_i = time.time()
+
+
+
+
+i_file_0_i = os.path.join(cwd, "i_file_of_all_character_possible_with_chr_in_python_0_i.txt")
+
+
+
+with open(i_file_0_i, "w") as f_:
+    
+    f_.write("")
+    
+
+
+
+i_result_str_0_i = "\n\n"
+
+
+
+#f_.write(i_result_str_0_i)
+    
+
+
+i_counter_1_i = 0
+
+
+i_counter_2_i = 0
+
+
+while (i_counter_1_i < 2_000_000):
+    
+    
+    try:
+        
+        
+        i_char_0_i = chr(i_counter_1_i)
+        
+        
+        
+        i_file_2_i = os.path.join(cwd, "i_space_1_i.txt")
+        
+        with open(i_file_2_i, "w") as f_:
+            
+            
+            f_.write(i_char_0_i)
+            
+            
+        
+        
+        
+        
+        with open(i_file_0_i, "a") as f_:
+            
+            f_.write(i_char_0_i)
+            
+        
+        
+        
+        i_counter_2_i += 1
+        
+        
+        
+            
+    except:
+        
+        
+        i_semaphore_0_i = True
+        
+        
+        
+        
+    
+    
+    
+    i_counter_1_i += 1
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+i_t_2_i = time.time()
+
+
+
+print(f"i_hello_0_i . time = {i_t_2_i - i_t_1_i} second . i_counter_2_i = {i_counter_2_i} .")
+
+
+
+
+'''
+
+1_112_064
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
