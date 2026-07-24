63d45fd8bef870f818d502f9c1a9957b923760d7
IPrincipalCentral1 <deboubil22@gmail.com>
2026-07-24 23:55:19 +0100

    i_hello_0_i

---
+++ i_nuclus/i_editor_of_code/edit-or_of_code/files_0/space_for_the_project/project_of_my_artificial_intelligence_of_bot_of_chat/i_compiler_of_i_python_i_0.py
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+this supposed to be working on Linux Ubuntu .
+
+
+
+
+
+'''
+
+
+
+import os
+
+import sys
+
+
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+
+
+
+folder_of_i_nuclus_0 = "i_nuclus_i_0"
+
+
+name_of_file_0 = "i_start_i_0.py"
+
+
+folder_0 = os.path.join(cwd, folder_of_i_nuclus_0)
+
+
+sys.path.append(folder_0)
+
+
+import i_start_i_0
+
+
+
+
+
+def main():
+
+    
+    i_start_i_0.i_main_i_0()
+    
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    
+    main()
+    
+    
+    
+
+
+
